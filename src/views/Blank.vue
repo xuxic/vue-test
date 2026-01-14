696097b129bce2b667a9b8d69eaffe2bbc564739
xuxicheng <290071060@qq.com>
2026-01-14 17:56:08 +0800

    test v-show

--- src/views/Blank.vue
+++ src/views/Blank.vue
@@ -3,13 +3,18 @@
     <div class="blank-content">
       <h1 class="text-center text-muted">空白页</h1>
       <p class="text-center text-muted">这是一个空白页面</p>
+      <Counter />
+      <button v-on:click="showMessage = !showMessage">显示/隐藏</button>
+      <p v-show="showMessage">Hello Vue!</p>
     </div>
-    <Counter />
   </div>
 </template>
 
 <script setup>
-import Counter from '../components/Counter.vue'
+import Counter from "../components/Counter.vue";
+import { ref } from "vue";
+
+const showMessage = ref(false);
 </script>
 
 <style scoped>
